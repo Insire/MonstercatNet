b9441a36037fa2e67ee1d5ddf285ca374bce761c
Peter Vietense <peter.vietense@hotmail.de>
2020-10-04 08:12:27 +0200

    updated changelog.md, endpoints.md
    expanded sample app to include missing attributes for report generation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,23 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this projects versioning scheme was inspired by [Semantic Versioning](https://semver.org/spec/v2.0.0.html) and can be found [here](versioning.md).
 
+## [2.0.22] - 2020-10-04
+
+### Added
+
+#### Endpoints
+
+- GET /self/playlists
+
+## [2.0.21] - 2020-10-04
+
+### Added
+
+#### Endpoints
+
+- POST /self/playlist
+- DELETE /playlist/{playlistId}
+
 ## [2.0.19] - 2020-08-20
 
 ### Added
--- MonstercatNet.Sample.Wpf/ReportGeneratorUtility.cs
+++ MonstercatNet.Sample.Wpf/ReportGeneratorUtility.cs
@@ -18,11 +18,17 @@ namespace MonstercatNet.Sample.Wpf
             var methods = type.GetMethods();
             var getAttributeType = typeof(GetAttribute);
             var postAttributeType = typeof(PostAttribute);
+            var deleteAttributeType = typeof(DeleteAttribute);
+            var putAttributeType = typeof(PutAttribute);
+            var patchAttributeType = typeof(PatchAttribute);
 
             foreach (var method in methods)
             {
                 attributes.AddRange((HttpMethodAttribute[])Attribute.GetCustomAttributes(method, getAttributeType));
                 attributes.AddRange((HttpMethodAttribute[])Attribute.GetCustomAttributes(method, postAttributeType));
+                attributes.AddRange((HttpMethodAttribute[])Attribute.GetCustomAttributes(method, deleteAttributeType));
+                attributes.AddRange((HttpMethodAttribute[])Attribute.GetCustomAttributes(method, putAttributeType));
+                attributes.AddRange((HttpMethodAttribute[])Attribute.GetCustomAttributes(method, patchAttributeType));
             }
 
             var builder = new StringBuilder();
