667feb20ae86c9954ec011c567b4ed6d848bae7d
Peter Vietense <peter.vietense@hotmail.de>
2022-02-20 15:10:13 +0100

    Renamed: Playlist -> GetPlaylistResult

--- MonstercatNet/Models/Playlist/GetPlaylist/GetPlaylistResult.cs
+++ MonstercatNet/Models/Playlist/GetPlaylist/GetPlaylistResult.cs
@@ -1,33 +1,28 @@
-using Newtonsoft.Json;
 using Refit;
 using System;
 
 namespace SoftThorn.MonstercatNet
 {
-    public sealed class Playlist
+    public sealed class GetPlaylistResult
     {
-        [AliasAs("Archived")]
-        public bool Deleted { get; set; }
+        public bool Archived { get; set; }
 
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public Guid Id { get; set; }
-        [AliasAs("IsPublic")]
-        [JsonProperty("IsPublic")]
-        public bool Public { get; set; }
+
+        public bool IsPublic { get; set; }
 
         public bool MyLibrary { get; set; }
         public int NumRecords { get; set; }
 
-        [AliasAs("Title")]
-        [JsonProperty("Title")]
-        public string Name { get; set; } = "";
-
+        public string Title { get; set; } = "";
         public string Description { get; set; } = "";
 
         public Guid UserId { get; set; }
+
         [AliasAs("Items")]
-        public PlaylistTrack[] Tracks { get; set; } = Array.Empty<PlaylistTrack>();
+        public GetPlaylistResultTrack[] Tracks { get; set; } = Array.Empty<GetPlaylistResultTrack>();
 
         public string TileFileId { get; set; } = "";
         public string BackgroundFileId { get; set; } = "";
