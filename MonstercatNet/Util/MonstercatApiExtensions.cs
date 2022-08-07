b4841ceaf0441892a97ceb7cce5e9865a7985655
Peter Vietense <peter.vietense@hotmail.de>
2022-08-07 18:40:36 +0200

    added cancellation support in MonstercatApiExtensions

--- MonstercatNet/Util/MonstercatApiExtensions.cs
+++ MonstercatNet/Util/MonstercatApiExtensions.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SoftThorn.MonstercatNet
@@ -8,9 +9,11 @@ namespace SoftThorn.MonstercatNet
         /// <summary>
         /// gold membership required
         /// </summary>
-        public static async Task<byte[]> DownloadTrackAsByteArray(this IMonstercatApi api, TrackDownloadRequest request)
+        public static async Task<byte[]> DownloadTrackAsByteArray(this IMonstercatApi api, TrackDownloadRequest request, CancellationToken token = default)
         {
-            var content = await api.DownloadTrack(request).ConfigureAwait(false);
+            var content = await api.DownloadTrack(request, token).ConfigureAwait(false);
+
+            token.ThrowIfCancellationRequested();
 
             return await content.ReadAsByteArrayAsync().ConfigureAwait(false);
         }
@@ -18,16 +21,20 @@ namespace SoftThorn.MonstercatNet
         /// <summary>
         /// gold membership required
         /// </summary>
-        public static async Task<Stream> DownloadTrackAsStream(this IMonstercatApi api, TrackDownloadRequest request)
+        public static async Task<Stream> DownloadTrackAsStream(this IMonstercatApi api, TrackDownloadRequest request, CancellationToken token = default)
         {
-            var content = await api.DownloadTrack(request).ConfigureAwait(false);
+            var content = await api.DownloadTrack(request, token).ConfigureAwait(false);
+
+            token.ThrowIfCancellationRequested();
 
             return await content.ReadAsStreamAsync().ConfigureAwait(false);
         }
 
-        public static async Task<Stream> StreamTrackAsStream(this IMonstercatApi api, TrackStreamRequest request)
+        public static async Task<Stream> StreamTrackAsStream(this IMonstercatApi api, TrackStreamRequest request, CancellationToken token = default)
         {
-            var content = await api.StreamTrack(request).ConfigureAwait(false);
+            var content = await api.StreamTrack(request, token).ConfigureAwait(false);
+
+            token.ThrowIfCancellationRequested();
 
             return await content.ReadAsStreamAsync().ConfigureAwait(false);
         }
